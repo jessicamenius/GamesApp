c0a69848e3e67e94fc72c33a03b26cf982d3e195
Julia Hanlon <jhanlon289@gmail.com>
2020-05-11 20:34:53 -0700

    added dark mode

--- app.js
+++ app.js
@@ -11,3 +11,22 @@ toggleBtn.addEventListener("click", function () {
     document.body.setAttribute("class", "light-mode");
   }
 });
+
+$(document).ready(function () {
+  console.log("hello");
+  $("#toggleBtn").on("click", function () {
+    if (toggleStatus === "toggle toggleFalse") {
+      $(".navbar").attr(
+        "class",
+        "navbar navbar-expand-lg navbar-dark bg-dark dark-mode"
+      );
+      $(".card").attr("class", "card dark-mode border-white");
+    } else {
+      $(".navbar").attr(
+        "class",
+        "navbar navbar-expand-lg navbar-light bg-light light-mode"
+      );
+      $(".card").attr("class", "card light-mode");
+    }
+  });
+});
