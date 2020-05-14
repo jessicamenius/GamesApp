a555be32b73147b309f30dcf564ea41de393772b
Julia Hanlon <jhanlon289@gmail.com>
2020-05-14 13:05:34 -0700

    added styling

--- app.js
+++ app.js
@@ -1,26 +1,37 @@
+var toggleBtn = document.querySelector("#toggleBtn");
+var toggleDisplay = document.querySelector("#toggleDisplay");
+var toggleStatus;
+toggleBtn.addEventListener("click", function () {
+  toggleStatus = toggleDisplay.getAttribute("class");
+  if (toggleStatus === "toggle toggleFalse") {
+    document.body.setAttribute("class", "dark-mode");
+    toggleDisplay.setAttribute("class", "toggle toggleTrue");
+  } else {
+    toggleDisplay.setAttribute("class", "toggle toggleFalse");
+    document.body.setAttribute("class", "light-mode");
+  }
+});
+
 $(document).ready(function () {
   console.log("hello");
-  if ($("#toggleDisplay").attr("class") === "toggle toggleFalse") {
-    $(".navbar").attr(
-      "class",
-      "navbar navbar-expand-lg navbar-dark bg-dark dark-mode"
-    );
-    $("body").attr("class", "dark-mode");
-    $("#toggleDisplay").attr("class", "toggle toggleTrue");
-    $(".card").attr("class", "card dark-mode border-white");
-  } else {
-    $(".navbar").attr(
-      "class",
-      "navbar navbar-expand-lg navbar-light light-mode"
-    );
-    $(".card").attr("class", "card light-mode");
-    $("body").attr("class", "light-mode");
-    $("#toggleDisplay").attr("class", "toggle toggleFalse");
-  }
+  $("#toggleBtn").on("click", function () {
+    if (toggleStatus === "toggle toggleFalse") {
+      $(".navbar").attr(
+        "class",
+        "navbar navbar-expand-lg navbar-dark bg-dark dark-mode"
+      );
+      $(".card").attr("class", "card dark-mode border-white");
+    } else {
+      $(".navbar").attr(
+        "class",
+        "navbar navbar-expand-lg navbar-light light-mode"
+      );
+      $(".card").attr("class", "card light-mode");
+    }
   });
 
   newJoke();
-// function to create random dad joke in the footer
+  // function to create random dad joke
   function newJoke() {
     $.ajax({
       type: "GET",
@@ -32,7 +43,7 @@ $(document).ready(function () {
       $("#joke").html(joke);
     });
   }
-// eventListener to create new dad joke on click
+  // event listener to create new dad joke
   $("#joke").on("click", function (e) {
     e.preventDefault();
     newJoke();
