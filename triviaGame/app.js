1a96e401824a623f61e40b25f08a3a8fa2bd3008
Jessica <jessicamenius@gmail.com>
2020-05-14 18:34:57 -0500

    fixing navbars

--- triviaGame/app.js
+++ triviaGame/app.js
@@ -76,8 +76,8 @@ $(document).ready(function () {
 
     function displayQuestion() {
       document.getElementById("question").innerHTML =
-        questions[currentQuestion].title;
-      for (var i = 0; i < questions[currentQuestion].choices.length; i += 1) {
+        question[currentQuestion].title;
+      for (var i = 0; i < question[currentQuestion].choices.length; i += 1) {
         var button = document.createElement("button");
         button.type = "button";
         button.value = i;
@@ -99,10 +99,6 @@ $(document).ready(function () {
           // move onto next question
           document.getElementById("answers").innerHTML = "";
         });
-        });
-  });
-});
-});
-});
-});
+        };
+  };
 });
