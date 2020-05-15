7fc3b53118aab51a09e9fe042ad386537054c613
Jim Mehta <jimit1@gmail.com>
2020-05-15 14:30:48 -0700

    some changes

--- highscores/app.js
+++ highscores/app.js
@@ -6,26 +6,43 @@ $(document).ready(function () {
   enterInitials();
   $("#toggleBtn").on("click", function () {
     if ($("#toggleDisplay").attr("class") === "toggle toggleFalse") {
-      $(".navbar").attr(
-        "class",
-        "navbar navbar-expand-lg navbar-dark bg-dark dark-mode"
-      );
-      $("body").attr("class", "dark-mode");
-      $("#toggleDisplay").attr("class", "toggle toggleTrue");
-      $(".card").attr("class", "card dark-mode border-white");
-      mode = "darkMode";
+      darkMode();
     } else {
-      $(".navbar").attr(
-        "class",
-        "navbar navbar-expand-lg navbar-light light-mode"
-      );
-      $(".card").attr("class", "card light-mode");
-      $("body").attr("class", "light-mode");
-      $("#toggleDisplay").attr("class", "toggle toggleFalse");
+      lightMode();
     }
-
-    window.localStorage.setItem("mode", mode);
   });
+
+  if (window.localStorage.getItem("mode") === "light-mode") {
+    lightMode();
+  }
+  if (window.localStorage.getItem("mode") === "dark-mode") {
+    darkMode();
+  }
+
+  function darkMode() {
+    window.localStorage.setItem("mode", "dark-mode");
+    $(".navbar").attr(
+      "class",
+      "navbar navbar-expand-lg navbar-dark bg-dark dark-mode fixed-top"
+    );
+    $("body").attr("class", "dark-mode");
+    $("#toggleDisplay").attr("class", "toggle toggleTrue");
+    $(".card").attr("class", "card dark-mode border-white mt-5");
+    $("#footer").attr("style", `background-color: #343A40; color: white;`);
+    $(".dropdwon-menu").attr("class");
+  }
+
+  function lightMode() {
+    window.localStorage.setItem("mode", "light-mode");
+    $(".navbar").attr(
+      "class",
+      "navbar navbar-expand-lg navbar-light light-mode fixed-top"
+    );
+    $(".card").attr("class", "card light-mode mt-5");
+    $("body").attr("class", "light-mode");
+    $("#toggleDisplay").attr("class", "toggle toggleFalse");
+    $("#footer").attr("style", `background-color: #a641c9; color: black`);
+  }
   newJoke();
 
   function newJoke() {
