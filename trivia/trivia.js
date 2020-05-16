16ef19f8ed84f50bd0d6da62eaf460d980e2ef9a
Jessica <jessicamenius@gmail.com>
2020-05-16 10:04:28 -0500

    trivia updates

--- trivia/trivia.js
+++ trivia/trivia.js
@@ -1,7 +1,16 @@
+var mode = "lightMode";
+
 $(document).ready(function () {
   var questions = [];
   var currentQuestion = 0;
   var score = 0;
+  var timer = 100;
+
+  var playgame = "War";
+  window.localStorage.setItem("playgame", playgame);
+
+  var playgame = "War";
+  window.localStorage.setItem("playgame", playgame);
 
   function shuffleChoices(array) {
     for (var i = array.length - 1; i > 0; i--) {
@@ -11,46 +20,46 @@ $(document).ready(function () {
       array[j] = temp;
     }
   }
+
   $("#submitBtn").click(function () {
     $("#submitBtn").hide();
-    $("#quiz").append(`
-    <div class="card text-center">
-    <div class="card-body" id="prompt_display">
-    <div id="question"></div>
-    <ul id="answers" class="list-group"></ul>
-    <p class="card-text"></p>
-    <div id="alert"></div>
-  </div>`);
-    $.ajax({
-      method: "GET",
-      url: "https://opentdb.com/api.php?amount=10&category=9&type=multiple",
-    }).then(function (res) {
-      for (var i = 0; i < res.results.length; i++) {
-        questions.push({
-          question: res.results[i].question,
-          answers: [
-            ...res.results[i].incorrect_answers,
-            res.results[i].correct_answer,
-          ],
-          correctAnswer: res.results[i].correct_answer,
-        });
-        shuffleChoices(questions[i].answers);
-      }
-      function displayQuestions() {
-        $("#question").html("");
-        $("#answers").html("");
-        $("#question").html(questions[currentQuestion].question);
-        for (var i = 0; i < questions[currentQuestion].answers.length; i++) {
-          $("#answers").append(
-            '<button onclick="verifyAnswer()" value="' +
-              questions[currentQuestion].answers[i] +
-              '">' +
-              questions[currentQuestion].answers[i] +
-              "</button>"
-          );
+    startTimer();
+
+
+    function doAjax(){
+      $.ajax({
+        method: "GET",
+        url: "https://opentdb.com/api.php?amount=10&category=9&type=multiple",
+      }).then(function (res) {
+        for (var i = 0; i < res.results.length; i++) {
+          questions.push({
+            question: res.results[i].question,
+            answers: [
+              ...res.results[i].incorrect_answers,
+              res.results[i].correct_answer,
+            ],
+            correctAnswer: res.results[i].correct_answer,
+          });
+          shuffleChoices(questions[i].answers);
         }
-      }
+
+        function displayQuestions() {
+          $("#question").html("");
+          $("#answers").html("");
+          $("#question").html(questions[currentQuestion].question);
+          for (var i = 0; i < questions[currentQuestion].answers.length; i++) {
+            $("#answers").append(
+              '<button onclick="verifyAnswer()" value="' +
+                questions[currentQuestion].answers[i] +
+                '">' +
+                questions[currentQuestion].answers[i] +
+                "</button>"
+            );
+          }
+        }
+
       displayQuestions();
+
       function verifyAnswer(event) {
         // compare the answer
         var userAnswer = event.target.value;
@@ -62,6 +71,7 @@ $(document).ready(function () {
           $("#score").append("Score: " + window.score);
         }
       }
-    });
+    }
+    );
   });
 });
