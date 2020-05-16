0f6d5e90cfba7131aa46418dc9dfd4142b9fbbae
Jessica <jessicamenius@gmail.com>
2020-05-16 00:17:47 -0500

    changes to navbar

--- nav.js
+++ nav.js
@@ -1,7 +1,5 @@
 $(document).ready(function () {
   $(".navbar").html(`    
-  <nav class="navbar navbar-expand-lg navbar-light light-mode fixed-top">
-
   <a class="navbar-brand" href=".././Index/index.html">Games App</a>
 <button
   class="navbar-toggler"
@@ -18,7 +16,7 @@ $(document).ready(function () {
 <div class="collapse navbar-collapse" id="navbarSupportedContent">
   <ul class="navbar-nav mr-auto">
     <li class="nav-item active">
-      <a class="nav-link" href=".././Index/index.html"
+      <a class="nav-link" href=".././index/index.html"
         >Home <span class="sr-only"></span></a
       >
     </li>
@@ -40,7 +38,7 @@ $(document).ready(function () {
           >Memory</a
         >
         <a class="dropdown-item" href="../trivia/trivia.html">Trivia</a>
-        <a class="dropdown-item" href="../Snake/snake.html">Snake</a>
+        <a class="dropdown-item" href="../snake/snake.html">Snake</a>
         <a class="dropdown-item" href="../reactiongame/reaction.html"
           >Reaction</a
         >
